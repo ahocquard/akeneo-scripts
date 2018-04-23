184d7e666b851c71519468450223b239222b56f4
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2018-04-23 17:36:27 +0200

    Fix script to import data

--- import_dump.sh
+++ import_dump.sh
@@ -6,7 +6,7 @@ export DATABASE_PASSWORD=$(cat /home/akeneo/pim/app/config/parameters.yml | grep
 mysqldump -u akeneo_pim -p${DATABASE_PASSWORD} --quick akeneo_pim | gzip
 COMMAND
 
-gunzip < /var/tmp/dump.sql.gz | mysql -u ${LOCAL_DATABASE_USER} -p${LOCAL_DATABASE_PASSWORD} ${LOCAL_DATABASE}
+gunzip < /var/tmp/dump.sql.gz | mysql -u ${LOCAL_DATABASE_USER} -p${LOCAL_DATABASE_PASSWORD} ${LOCAL_DATABASE_NAME}
 cd ${PIM_DIRECTORY}
 bin/console akeneo:elasticsearch:reset-indexes
 bin/console pim:product-model:index --all
