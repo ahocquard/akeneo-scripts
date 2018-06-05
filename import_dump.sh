ba6a066785dc1e035bc1be8dcb249dc574565e83
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2018-06-05 15:40:40 +0200

    Fix problem by gzipping on the fly the dump

--- import_dump.sh
+++ import_dump.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-ssh akeneo@${SERVER_URI} >/var/tmp/dump.sql.gz <<'COMMAND'
+ssh akeneo@${SERVER_URI} <<'COMMAND'
 
 export DATABASE_PASSWORD=$(cat /home/akeneo/pim/app/config/parameters.yml | grep database_password | sed 's/database_password://g' | tr -d ' ')
-mysqldump -u akeneo_pim -p${DATABASE_PASSWORD} --quick akeneo_pim | gzip
+mysqldump -u akeneo_pim -p${DATABASE_PASSWORD} --quick akeneo_pim | gzip >/var/tmp/dump.sql.gz
 COMMAND
+
+scp akeneo@${SERVER_URI}:/var/tmp/dump.sql.gz /var/tmp/dump.sql.gz
 
 gunzip < /var/tmp/dump.sql.gz | mysql -u ${LOCAL_DATABASE_USER} -p${LOCAL_DATABASE_PASSWORD} ${LOCAL_DATABASE_NAME}
 cd ${PIM_DIRECTORY}
