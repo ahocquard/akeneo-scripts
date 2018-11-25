6ae0ce46f2f2d43be631032c95aaad812f3b46e1
ahocquard <alexandre.hocquard@akeneo.com>
2018-11-25 17:36:46 +0100

    Avoid to doanload the versioning and the warning

--- README.md
+++ README.md
@@ -13,7 +13,15 @@ export SERVER_URI=customer-sds.cloud.akeneo.com
 export LOCAL_DATABASE_USER=akeneo_pim
 export LOCAL_DATABASE_PASSWORD=akeneo_pim
 export LOCAL_DATABASE_NAME=akeneo_pim
+export LOCAL_DATABASE_PORT=33006
+export LOCAL_DATABASE_HOST=127.0.0.1
 export PIM_DIRECTORY=/home/akeneo/pim
 
 ./import_dump.sh
+```
+
+If you have an error to import the dump because Mysql disconnects, execute this command before importing the dump: 
+
+```
+SET GLOBAL max_allowed_packet=1073741824;
 ```
--- import_dump.sh
+++ import_dump.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ssh akeneo@${SERVER_URI} <<'COMMAND'
-
 export DATABASE_PASSWORD=$(cat /home/akeneo/pim/app/config/parameters.yml | grep database_password | sed 's/database_password://g' | tr -d ' ')
-mysqldump -u akeneo_pim -p${DATABASE_PASSWORD} --quick akeneo_pim | gzip >/var/tmp/dump.sql.gz
+mysqldump --ignore-table=akeneo_pim.pim_versioning_version --ignore-table=akeneo_pim.akeneo_batch_warning -u akeneo_pim -p${DATABASE_PASSWORD} --quick akeneo_pim | gzip >/var/tmp/dump.sql.gz
 COMMAND
 
 scp akeneo@${SERVER_URI}:/var/tmp/dump.sql.gz /var/tmp/dump.sql.gz
 
-gunzip < /var/tmp/dump.sql.gz | mysql -u ${LOCAL_DATABASE_USER} -p${LOCAL_DATABASE_PASSWORD} ${LOCAL_DATABASE_NAME}
+gunzip < /var/tmp/dump.sql.gz | mysql -u ${LOCAL_DATABASE_USER} -p${LOCAL_DATABASE_PASSWORD} ${LOCAL_DATABASE_NAME} -P ${LOCAL_DATABASE_PORT} -h ${LOCAL_DATABASE_HOST}
 cd ${PIM_DIRECTORY}
-bin/console akeneo:elasticsearch:reset-indexes
-bin/console pim:product-model:index --all
-bin/console pim:product:index --all
+docker-compose exec fpm bin/console akeneo:elasticsearch:reset-indexes -e prod
+docker-compose exec fpm bin/console pim:product-model:index --all -e prod
+docker-compose exec fpm bin/console pim:product:index --all -e prod
